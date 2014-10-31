d767ad9a8b6af1bd7cf10e3962d1aa4dfe8f3391
Guilherme Taschetto <gtaschetto@gmail.com>
2014-10-31 17:55:04 -0200

    Add data aliases and terminal colors.

---
+++ t2/colors.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Colors
+{
+  const std::string red = "\033[1;31m";
+  const std::string green = "\033[1;32m";
+  const std::string yellow = "\033[1;33m";
+  const std::string blue = "\033[1;34m";
+  const std::string reset = "\033[0m";
+}
+
