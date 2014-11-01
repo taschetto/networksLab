f482ef836eccfe7e3604c46575879bfaa9b24325
Guilherme Taschetto <gtaschetto@gmail.com>
2014-11-01 12:11:58 -0200

    Organizado código base para o 'sniffer'.

--- t2/helpers.h
+++ t2/helpers.h
@@ -1,26 +1,4 @@
 #pragma once
 
-#include <string>
-#include "defines.h"
-
 void ok();
 void error();
-
-void MakeMAC(const BYTE*, BYTE*);
-void MakeIP(const BYTE*, BYTE*);
-
-WORD MakeWord(const BYTE*);
-DWORD MakeDWord(const BYTE*);
-
-std::string AddrToStr(const BYTE*, BYTE);
-
-std::string MACToStr(const BYTE*);
-std::string IPToStr(const BYTE*);
-
-BYTE LoByte(const WORD);
-BYTE HiByte(const WORD);
-
-BYTE LoNibble(const BYTE*);
-BYTE HiNibble(const BYTE*);
-
-bool CompareIP(const BYTE*, const BYTE*);
--- t2/phantom.cpp
+++ t2/phantom.cpp
@@ -1,10 +1,49 @@
+#include <cstring>
 #include <iostream>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
 #include "colors.h"
+#include "interface.h"
 
 using namespace std;
 using namespace Colors;
 
+void sigint_handler(int);
+
 int main(int argc, char* argv[])
 {
-  cout << green << "Hello world!" << reset << endl;
+  if (argc != 2)
+  {
+    std::cerr << "Usage: phantom <interface>" << endl;
+    exit(-1); 
+  }
+
+  if (initSignal(sigint_handler) < 0)
+    exit(-1);
+
+  int socket;
+  if (initSocket(socket) < 0)
+    exit(-1);
+
+  struct ifreq ifr;
+  strcpy(ifr.ifr_name, argv[1]);
+
+  if (initInterface(socket, ifr) < 0)
+    exit(-1);
+
+  // Ready to capture. :-)
+
+  if (resetInterface(socket, ifr) < 0)
+    exit(-1);
+
+  if (closeSocket(socket) < 0)
+    exit(-1);
+
+  return 0;
+}
+
+void sigint_handler(int)
+{
 }
