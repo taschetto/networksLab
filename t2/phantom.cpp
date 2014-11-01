151ba2d615db80e6f01952b88758101010b74dfc
Guilherme Taschetto <gtaschetto@gmail.com>
2014-11-01 14:01:03 -0200

    Add sniffing for ethernet & ip packets.

--- t2/phantom.cpp
+++ t2/phantom.cpp
@@ -1,8 +1,11 @@
 #include <cstring>
+
 #include <iostream>
+#include <thread>
+
 #include <net/ethernet.h>
 #include <netinet/ether.h>
-#include <netinet/ip.h>
+#include <linux/ip.h>
 #include <netinet/udp.h>
 #include <arpa/inet.h>
 
@@ -13,6 +16,9 @@ using namespace std;
 using namespace Colors;
 
 void sigint_handler(int);
+void sniff(const int, const ifreq&);
+
+bool stop = false;
 
 int main(int argc, char* argv[])
 {
@@ -35,11 +41,8 @@ int main(int argc, char* argv[])
   if (initInterface(socket, ifr) < 0)
     exit(-1);
 
-  // Ready to capture. :-)
-  cout << blue << "Ready to capture from: " << reset << ifr.ifr_name << endl;
-  cout << blue << "      Interface Index: " << reset << ifr.ifr_ifindex << endl;
-  cout << blue << "        Hardware Addr: " << reset << ether_ntoa((struct ether_addr*)ifr.ifr_hwaddr.sa_data) << endl;
-  cout << blue << "        Protocol Addr: " << reset << inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr) << endl;
+  std::thread sniffer(sniff, socket, ifr);
+  sniffer.join();
 
   if (resetInterface(socket, ifr) < 0)
     exit(-1);
@@ -52,4 +55,59 @@ int main(int argc, char* argv[])
 
 void sigint_handler(int)
 {
+  cout << yellow << "Received SIGINT to interrupt execution." << reset << endl;
+  stop = true;
+}
+
+void sniff(const int socket, const ifreq& ifr)
+{
+  cout << blue << "Ready to capture from: " << reset << ifr.ifr_name << endl;
+  cout << blue << "      Interface Index: " << reset << ifr.ifr_ifindex << endl;
+  cout << blue << "        Hardware Addr: " << reset << ether_ntoa((struct ether_addr*)ifr.ifr_hwaddr.sa_data) << endl;
+  cout << blue << "        Protocol Addr: " << reset << inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr) << endl;
+
+  u_char buff[ETHER_MAX_LEN];
+
+  for (;;)
+  {
+    if (stop)
+      break;
+
+    if (recv(socket, (char*)&buff, sizeof(buff), 0x00) < ETHER_MIN_LEN)
+      continue;
+
+    struct ether_header ether;
+    memcpy(&ether, &buff, sizeof(ether));
+      
+    cout << endl;
+    cout << blue << "Ethernet Packet" << reset << endl;
+    cout << blue << "Destination: " << reset << ether_ntoa((struct ether_addr*)ether.ether_dhost) << endl;
+    cout << blue << "        Src: " << reset << ether_ntoa((struct ether_addr*)ether.ether_shost) << endl;
+    cout << blue << "       Type: " << reset << ntohs(ether.ether_type) << endl;
+
+    if (ntohs(ether.ether_type) == ETHERTYPE_IP)
+    {
+      struct iphdr ip;
+      memcpy(&ip, &buff[ETHER_HDR_LEN], sizeof(iphdr));
+
+      cout << endl;
+      cout << blue << "IP Packet" << reset << endl;
+      cout << blue << "        IHL: " << reset << ip.ihl << endl;
+      cout << blue << "    Version: " << reset << ip.version << endl;
+      cout << blue << "        TOS: " << reset << ip.tos << endl;
+      cout << blue << "        LEN: " << reset << ip.tot_len << endl;
+      cout << blue << "         ID: " << reset << ip.id << endl;
+      cout << blue << "   Frag Off: " << reset << ip.frag_off << endl;
+      cout << blue << "        TTL: " << reset << ip.ttl << endl;
+      cout << blue << "   Protocol: " << reset << ntohs(ip.protocol) << endl;
+      cout << blue << "      Check: " << reset << ip.check << endl;
+      cout << blue << "     Source: " << reset << inet_ntoa(((struct sockaddr_in *)&ip.saddr)->sin_addr) << endl;
+      cout << blue << "Destination: " << reset << inet_ntoa(((struct sockaddr_in *)&ip.daddr)->sin_addr) << endl;
+
+      if (ntohs(ip.protocol) == IPPROTO_UDP)
+      {
+        cout << endl << red << "Received UDP packet!" << reset << endl;
+      }
+    }
+  }
 }
